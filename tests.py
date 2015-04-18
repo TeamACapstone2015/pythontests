645e5304aae02fffc74faa4c404194861b661df5
Cameron White <cameron.white@jivesoftware.com>
2015-04-17 18:47:28 -0700

    My new test

--- tests.py
+++ tests.py
@@ -8,5 +8,8 @@ class tests(unittest.TestCase):
     def test_pass2(self):
         self.assertEqual(1, 1)
 
+    def test_pass3(self):
+        self.assertEqual(1, 1)
+
     def test_fail(self):
         self.assertEqual(1, 2)
