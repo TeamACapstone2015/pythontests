49771b7c180336ec7eff1ea08fdf081f8bb6edbc
Andrew <amcgown@pdx.edu>
2015-04-18 18:23:18 -0700

    Fixing
    not sure why these lines didnt commit...

--- tests.py
+++ tests.py
@@ -17,6 +17,8 @@ class tests(unittest.TestCase):
     def test_lowercase(self):
         self.assertTrue('asasha'.islower())
 		
+	def test_pass4(self):
 		self.assertGreater(2, 1)
 		
+	def test_fail2(self):
 		self.assertGreater(1, 1)
